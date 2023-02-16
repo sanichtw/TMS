76947f02839805034702010a61f21fdc1729c774
Nikita Kozlov <sanichtw@gmail.com>
2023-02-16 18:11:20 +0300

    fixed my homework03

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        checkNumber(); //  №1. Вывести на экран сколько в этом числе цифр и положительное оно или отрицательное.
+        Scanner scanner = new Scanner(System.in);
 
-        doesATriangleExist(); // №2. Определить существует ли такой треугольник.
+        checkNumber(scanner); //  №1. Вывести на экран сколько в этом числе цифр и положительное оно или отрицательное.
 
-        incrementOrDecrementInt(); // №3. Дано целое число. Если оно является положительным, то прибавить к нему 1.
+        doesATriangleExist(scanner); // №2. Определить существует ли такой треугольник.
+
+        incrementOrDecrementInt(scanner); // №3. Дано целое число. Если оно является положительным, то прибавить к нему 1.
         // Если отрицательным, то вычесть из него 2. Если нулевым, то заменить его на10. Вывести полученное число.
 
-        findPositiveIntegers(); // №4. Даны 3 целых числа. Найти количество положительных чисел в исходном наборе.
+        findPositiveIntegers(scanner); // №4. Даны 3 целых числа. Найти количество положительных чисел в исходном наборе.
 
-        countTheNegativeAndPositiveInt(); // №5. Даны 3 целых числа. Найти количество положительных и отрицательных чисел
+        countTheNegativeAndPositiveInt(scanner); // №5. Даны 3 целых числа. Найти количество положительных и отрицательных чисел
         // в исходном наборе.
 
-        printALargerNumber(); // №6. Даны 2 числа. Вывести большее из них.
+        printALargerNumber(scanner); // №6. Даны 2 числа. Вывести большее из них.
 
-        countProgrammes(); // №7. В зависимости от количества программистов необходимо вывести правильно окончание.
+        countProgrammes(scanner); // №7. В зависимости от количества программистов необходимо вывести правильно окончание.
     }
 
+
      // №1. Вывести на экран сколько в этом числе цифр и положительное оно или отрицательное.
-    static void checkNumber() {
-        Scanner scanner = new Scanner(System.in);
+    static void checkNumber(Scanner sc) {
         System.out.print("№1. Enter a number to find out how many digits it has and whether it is positive: ");
-        int num = scanner.nextInt();
+        int num = sc.nextInt();
         System.out.println(num);
 
         if (num < 0) {
@@ -39,13 +41,12 @@ public class Main {
 
 
     // №2. Определить существует ли такой треугольник
-    static void doesATriangleExist() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("№2. Enter a number to determine if such a triangle exists: ");
-        int sideA = scanner.nextInt();
-        int sideB = scanner.nextInt();
-        int sideC = scanner.nextInt();
-        if ((sideA > sideB + sideC) || (sideB > sideA + sideC) || (sideC > sideA + sideB)) {
+    static void doesATriangleExist(Scanner sc) {
+        System.out.print("№2. Enter three number to determine if such a triangle exists: ");
+        int sideA = sc.nextInt();
+        int sideB = sc.nextInt();
+        int sideC = sc.nextInt();
+        if ((sideA >= sideB + sideC) || (sideB >= sideA + sideC) || (sideC >= sideA + sideB)) {
             System.out.println("There is no such triangle");
         } else {
             System.out.println("Such a triangle exists");
@@ -55,10 +56,9 @@ public class Main {
 
     // №3. Дано целое число. Если оно является положительным, то прибавить к нему 1.
     // Если отрицательным, то вычесть из него 2. Если нулевым, то заменить его на 10. Вывести полученное число.
-    static void incrementOrDecrementInt() {
-        Scanner scanner = new Scanner(System.in);
+    static void incrementOrDecrementInt(Scanner sc) {
         System.out.print("№3. Enter an integer: ");
-        int number = scanner.nextInt();
+        int number = sc.nextInt();
 
         if (number > 0) {
             number++;
@@ -74,10 +74,9 @@ public class Main {
 
 
     // №4. Даны 3 целых числа. Найти количество положительных чисел в исходном наборе.
-    static void findPositiveIntegers() {
-        Scanner scanner = new Scanner(System.in);
+    static void findPositiveIntegers(Scanner sc) {
         System.out.print("№4. Enter 3 positive integers to find the positive ones: ");
-        int[] nums = {scanner.nextInt(), scanner.nextInt(), scanner.nextInt()};
+        int[] nums = {sc.nextInt(), sc.nextInt(), sc.nextInt()};
         int positiveNums = 0;
 
         for (int num : nums) {
@@ -91,10 +90,9 @@ public class Main {
 
     // №5. Даны 3 целых числа. Найти количество положительных и отрицательных чисел
     // в исходном наборе.
-    static void countTheNegativeAndPositiveInt() {
-        Scanner scanner = new Scanner(System.in);
+    static void countTheNegativeAndPositiveInt(Scanner sc) {
         System.out.print("№5. Enter 3 integers to find the number of positive and negative: ");
-        int[] nums = {scanner.nextInt(), scanner.nextInt(), scanner.nextInt()};
+        int[] nums = {sc.nextInt(), sc.nextInt(), sc.nextInt()};
         int positiveNums, negativeNums;
         positiveNums = negativeNums = 0;
 
@@ -112,11 +110,10 @@ public class Main {
 
 
     // №6. Даны 2 числа. Вывести большее из них
-    static void printALargerNumber() {
-        Scanner scanner = new Scanner(System.in);
+    static void printALargerNumber(Scanner sc) {
         System.out.print("№6. Enter 2 integers to find the larger one: ");
-        int num1 = scanner.nextInt();
-        int num2 = scanner.nextInt();
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
 
         System.out.printf("Наибольшее значение: %d", (num1 >= num2 ? num1 : num2));
         System.out.println();
@@ -124,10 +121,9 @@ public class Main {
 
 
     // №7. В зависимости от количества программистов необходимо вывести правильно окончание
-    static void countProgrammes() {
-        Scanner scanner = new Scanner(System.in);
+    static void countProgrammes(Scanner sc) {
         System.out.print("№7. Enter the number of programmers: ");
-        String num = scanner.nextLine();
+        String num = sc.next();
         char[] chars = {'2', '3', '4'};
 
         if ((num.length() >= 3 && num.substring(num.length() - 2).equals("01")) ||
@@ -145,6 +141,6 @@ public class Main {
                 }
             }
         }
-        scanner.close();
+
     }
 }
