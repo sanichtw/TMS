3c19886fc8393a4c041a4185a8cc128cfcbe99a1
Nikita Kozlov <sanichtw@gmail.com>
2023-03-28 01:26:34 +0300

    commit

--- src/PartTwo/PartTwo.java
+++ src/PartTwo/PartTwo.java
@@ -2,17 +2,38 @@ package PartTwo;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class PartTwo {
 
     // 1.
     public static void task1() {
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            Random random = new Random();
-            arrayList.add(random.nextInt(21));
+        ArrayList<Integer> randomNumbers = new ArrayList<>();
+
+        System.out.println("Удалить дубликаты");
+        for (int i = 0; i < 15; i++) {
+            randomNumbers.add(new Random().nextInt(21));
         }
-        System.out.println(arrayList);
-        System.out.println(arrayList.stream().distinct());
+        System.out.println(randomNumbers);
+        System.out.println(randomNumbers.stream().distinct().collect(Collectors.toList()));
+
+
+        System.out.println("\nВывести все четные элементы в диапазоне от 7 до 17 (включительно)");
+        System.out.println(randomNumbers
+                .stream()
+                .filter(i -> i >= 7 && i <= 17 && i % 2 == 0)
+                .collect(Collectors.toList()));
+
+
+        System.out.println("\nКаждый элемент умножить на 2");
+        System.out.println(randomNumbers
+                .stream()
+                .map(i -> i * 2)
+                .collect(Collectors.toList())
+        );
+
+
+        System.out.println("\nОтсортировать и вывести на экран первых 4 элемента");
+        System.out.println(randomNumbers.stream().limit(4).collect(Collectors.toList()));
     }
 }
